94abd8d50412b155318390ca2031813a9da92e89
Jonny Williams <jonny.williams@niwa.co.nz>
2023-12-05 14:33:53 +1300

    works now i think

--- go.py
+++ go.py
@@ -1,9 +1,18 @@
 import linecache
 import random
+import os
+
+os.chdir(os.path.expanduser('~/randomtarot'))
+
+print(os.getcwd())
 
 i = random.randint(1,78)
 
 print('your random tarot card is...\n')
 
-print(linecache.getline('cards.txt', int(i)))
+linenumber = linecache.getline('cardnames.txt', int(i))
+
+thecard = str(linecache.getline('cardfiles.txt', int(i)))
+
+os.system('display '+thecard)
 
