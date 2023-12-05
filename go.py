fc45c18ce2acc06b1f531da0e87232a8e62d8a22
Jonny Williams <jonny.williams@niwa.co.nz>
2023-12-05 16:22:52 +1300

    ascii

--- go.py
+++ go.py
@@ -11,10 +11,16 @@ i = random.randint(1,78)
 print('your random tarot card is...\n')
 
 linenumber = linecache.getline('cardnames.txt', int(i))
-print(linenumber)
+print(str(linenumber))
 
 thecard = str(linecache.getline('cardfiles.txt', int(i)))
 print(thecard)
+
+greppattern = 'grep -i -m 1 -A 21 ' + '\''+str(linenumber[0:-1])+'\'' + ' cards.js'
+
+os.system(greppattern+'|tail -20') 
+
+print(greppattern)
 
 os.system('display '+thecard)
 
